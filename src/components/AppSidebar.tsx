19a7f59d0299c6567f49ac92e69773068308e374
Diego <diegoalt.g@hotmail.com>
2025-11-09 22:10:34 -0300

    completando info sidebar

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -5,19 +5,50 @@ import {
   SidebarGroup,
   SidebarHeader,
 } from "@/components/ui/sidebar"
+import Link from "next/link"
+import { FaGithub, FaLinkedinIn } from "react-icons/fa"
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip"
+import { SiGmail } from "react-icons/si"
+import { useTranslations } from "next-intl"
  
 export function AppSidebar() {
+  const t = useTranslations("Sidebar")
   return (
     <Sidebar>
       <SidebarHeader>
-        <h3 className="mt-5 text-2xl text-center">Ponte en contacto</h3>
+        <h3 className="mt-5 text-2xl px-5">{t("titulo")}</h3>
         <hr className=" bg-lime-500"/>
       </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+      <SidebarContent className="px-5">
+        <p className=" mt-10 ">{t("descripcion")}</p>
+        <div className="mt-10">
+
+          <h4 className="text-zinc-400 text-xl">{t("numeroContacto")}</h4>
+          <p className="text-gray-600 text-lg">+569 54714609</p>
+
+          <h4 className="text-zinc-400 text-xl mt-4">Email</h4>
+          <p className="text-gray-600 text-lg">diegoaltdev@gmail.com</p>
+
+          <h4 className="text-zinc-400 text-xl mt-4">{t("ubicacion")}</h4>
+          <p className="text-gray-600 text-lg">Chile - Viña del mar</p>
+
+          <h4 className="text-zinc-400 text-xl mt-4">{t("redes")}</h4>
+          <div className="flex  gap-5 mt-2">
+
+          <Link href={"https://github.com/DGMLX"} target='_blank'><FaGithub className="text-xl cursor-pointer hover:text-lime-400 transition-all duration-400"/></Link>
+            <Link href={"https://www.linkedin.com/in/diego-altamirano-gallardo-72a7a2203/"} target='_blank'><FaLinkedinIn className="text-xl cursor-pointer hover:text-lime-400 transition-all duration-400"/></Link>
+            <Tooltip>
+            <TooltipTrigger>   <SiGmail className="text-xl hover:text-lime-400 transition-all duration-400"/></TooltipTrigger>
+            <TooltipContent>
+                <p>diegoaltdev@gmail.com</p>
+            </TooltipContent>
+            </Tooltip>
+          </div>
+        </div>
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <p className="text-gray-600 text-center pb-5">DGMLX - 2025</p>
+      </SidebarFooter>
     </Sidebar>
   )
 }
